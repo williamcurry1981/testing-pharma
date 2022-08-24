f788b2b2b72d99321b6f335ddec83699dc798745
Kazi Rayhan <rayhanbd4400@gmail.com>
2022-08-24 22:16:45 +0600

    add footer

--- src/component/footer/Footer.jsx
+++ src/component/footer/Footer.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import './footer.css'
+import footerImg from '../../assets/footer_logo.png';
+
 const Footer = () => {
   return (
     <footer>
       <div className="container">
-       <ul>
-        <li></li>
-       </ul>
+        <ul className="footer-nav-items">
+          <li>HOW WE OPERATE</li>
+          <li>CONTACT US TO PURCHASE</li>
+          <li>VERIFY A LAB TEST</li>
+        </ul>
+        <div className="footer-img-box">
+          <img src={footerImg} alt=""/>
+        </div>
+        <p>© TESTING PHARMA 2022</p>
+        <p>Saavedra 542, Santa Cruz de la Sierra, Bolivia</p>
       </div>
     </footer>
   )
