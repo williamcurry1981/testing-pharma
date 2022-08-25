6bfd16a4336b46675ca3c513a13117166e17b4b1
Kazi Rayhan <rayhanbd4400@gmail.com>
2022-08-25 23:01:47 +0600

    add delay in error msg

--- src/component/section/labTestVerification/LabTest.jsx
+++ src/component/section/labTestVerification/LabTest.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './labtest.css';
 import successIcon from '../../../assets/success.png';
 import errIcon from '../../../assets/error.png';
@@ -6,27 +6,28 @@ import { orderNumber } from '../../../db/db';
 
 const LabTest = () => {
     const [msg, setNotification] = useState(0);
-
-    const handleSubmit = (e) => {
-        setNotification(0)
-        e.preventDefault()
-        let find = orderNumber.filter(item => item.toLowerCase() == e.target["search-test"].value.toLowerCase()).length > 0;
-        if (find) {
-            setNotification(200);
-            setTimeout(() => {
-                setNotification(0);
-                e.target["search-test"].value = ""
-            }, 3000)
+    const [search, setSreach] = useState('');
+    const [err, setErr] = useState(false);
+    const handleSubmit = () => {
+        if (search== "") {
+            setErr(true);
+            setTimeout(() => setErr(false), 4000)
         }
         else {
-            setNotification(401);
-            setTimeout(() => {
-                setNotification(0);
-                e.target["search-test"].value = ""
-            }, 3000)
-
+            let find = orderNumber.filter(item => item.toLowerCase() == search.toLowerCase()).length > 0;
+            if (find) {
+                setNotification(200);
+            }
+            else {
+                setNotification(401);
+            }
         }
     }
+    useEffect(()=>{
+        if(search==""){
+            setNotification(0)
+        }
+    },[search])
     return (
         <section id="verify-lab-test" className="lab-test">
             <div className="section-title">
@@ -37,12 +38,17 @@ const LabTest = () => {
             <div className="labTest-content-wrapper">
                 <div className="container">
                     <div className="text-box">
-                        <p>Copy the ORDER NUMBER (seen in the top left part of the lab test) in the area below. Then click the “VERIFY LAB TEST” button. A verified or rejected message will be shown. All information on this website is directly connected to our database. If you believe there is an error, you may contact us. For new lab tests, allow up to 24 hours after test results have been emailed for it to properly verify on the website.</p>
+                        <p>Copy the ORDER NUMBER (seen in the top left part of the lab test) in the area below. Then click the “VERIFY LAB TEST” button. A verified or rejected message will be shown. The customer name shown below should match the name on the report, too. If you believe there is an error, you may contact us. For new lab tests, allow up to 24 hours after test results have been emailed for it to properly verify on the website.</p>
                     </div>
-                    <form onSubmit={handleSubmit} className="verify-test">
-                        <input name="search-test" type="text" required placeholder='Copy Order Number Here ' />
-                        <button>{"VERIFY LAB TEST (CLICK ME)"}</button>
-                    </form>
+                    <div className="verify-test">
+                        <input onChange={(e) => setSreach(e.target.value)} name="search-test" type="text" placeholder='Copy Order Number Here ' />
+                        <button onClick={() => handleSubmit()}>{"VERIFY LAB TEST (CLICK ME)"}</button>
+                        {err && <div className="lab-emty-field-notification">
+                            <p>{`Please fill out the field.`}</p>
+                        </div>
+                        }
+                    </div>
+
                     {msg === 200 && <div className="lab-notification">
                         <div className="lab-notify-icon">
                             <img src={successIcon} alt="" />
