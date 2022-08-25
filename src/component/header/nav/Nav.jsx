2c75268b51cc52bb12d7d3ba5093f7f40b018163
Kazi Rayhan <rayhanbd4400@gmail.com>
2022-08-25 11:52:22 +0600

    add mobile menu

--- src/component/header/nav/Nav.jsx
+++ src/component/header/nav/Nav.jsx
@@ -1,25 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './nav.css'
 import Logo from '../../../assets/testing_pharma.png';
 import { pageScroll } from '../../../Utils/PageScroll';
+import openIcon from '../../../assets/open_menu.svg';
+import closeIcon from '../../../assets/close_menu.svg';
 
 
 const Nav = () => {
-
+    const [open, setOpen] = useState(false)
     return (
         <nav className='header-navbar'>
-            <div className="container">
-                <div className="header-nav-wrapper  flex align_center justify_between">
-                <div className="nav-logo" onClick={() => {
-                    window.scrollTo(0, 0);
-                }}>
-                    <img src={Logo} alt="testing-pharma" />
-                </div>
-                <ul className="nav-items">
-                    <li onClick={() => pageScroll("how-operate")}>HOW WE OPERATE</li>
-                    <li onClick={() => pageScroll("contact")}>CONTACT US TO PURCHASE</li>
-                    <li onClick={() => pageScroll("verify-lab-test")}>VERIFY A LAB TEST</li>
-                </ul>
+            <div className="container nav-container">
+                <div className="header-nav-wrapper">
+                    <div className="nav-logo-wrapper">
+                        <div className="nav-logo" onClick={() => {
+                            window.scrollTo(0, 0);
+                        }}>
+                            <img src={Logo} alt="testing-pharma" />
+                        </div>
+                        <div className="hamgur-bar" onClick={() => setOpen(!open)}>
+                            <img src={open ? closeIcon : openIcon} alt="testing pharma" />
+                        </div>
+                    </div>
+                    <ul className={`nav-items ${open && "nav-items-active"}`}>
+                        <li onClick={() => { setOpen(!open); pageScroll("how-operate") }}>HOW WE OPERATE</li>
+                        <li onClick={() => { setOpen(!open); pageScroll("contact") }}>CONTACT US TO PURCHASE</li>
+                        <li onClick={() => { setOpen(!open); pageScroll("verify-lab-test") }}>VERIFY A LAB TEST</li>
+                    </ul>
                 </div>
             </div>
         </nav>
--- src/component/section/labTestVerification/LabTest.jsx
+++ src/component/section/labTestVerification/LabTest.jsx
@@ -10,7 +10,7 @@ const LabTest = () => {
     const handleSubmit = (e) => {
         setNotification(0)
         e.preventDefault()
-        let find = orderNumber.filter(item => item.orderNumber == e.target["search-test"].value).length > 0;
+        let find = orderNumber.filter(item => item.toLowerCase() == e.target["search-test"].value.toLowerCase()).length > 0;
         if (find) {
             setNotification(200);
             setTimeout(() => {
@@ -29,33 +29,33 @@ const LabTest = () => {
     }
     return (
         <section id="verify-lab-test" className="lab-test">
-          <div className="section-title">
-              <div className="container">
-                  <h2 className="title">VERIFY A LAB TEST HERE</h2>
-              </div>
-          </div>
-          <div className="labTest-content-wrapper">
-            <div className="container">
-                <div className="text-box">
-                    <p>Copy the ORDER NUMBER (seen in the top left part of the lab test) in the area below. Then click the “VERIFY LAB TEST” button. A verified or rejected message will be shown. All information on this website is directly connected to our database. If you believe there is an error, you may contact us. For new lab tests, allow up to 24 hours after test results have been emailed for it to properly verify on the website.</p>
+            <div className="section-title">
+                <div className="container">
+                    <h2 className="title">VERIFY A LAB TEST HERE</h2>
                 </div>
-                <form onSubmit={handleSubmit} className="verify-test">
-                    <input name="search-test" type="text" required placeholder='Copy Order Number Here ' />
-                    <button>{"VERIFY LAB TEST (CLICK ME)"}</button>
-                </form>
-                {msg === 200 && <div className="lab-notification">
-                    <div className="lab-notify-icon">
-                        <img src={successIcon} alt="" />
-                    </div>
-                    <h4>THIS LAB TEST IS VERIFIED.</h4>
-                </div>}
-                {msg === 401 && <div className="lab-notification lab-notification-error">
-                    <div className="lab-notify-icon">
-                        <img src={errIcon} alt="" />
-                    </div>
-                    <h4>{`THE ORDER NUMBER IS REJECTED. THIS DOESN’T EXIST.`}</h4>
-                </div>}
             </div>
+            <div className="labTest-content-wrapper">
+                <div className="container">
+                    <div className="text-box">
+                        <p>Copy the ORDER NUMBER (seen in the top left part of the lab test) in the area below. Then click the “VERIFY LAB TEST” button. A verified or rejected message will be shown. All information on this website is directly connected to our database. If you believe there is an error, you may contact us. For new lab tests, allow up to 24 hours after test results have been emailed for it to properly verify on the website.</p>
+                    </div>
+                    <form onSubmit={handleSubmit} className="verify-test">
+                        <input name="search-test" type="text" required placeholder='Copy Order Number Here ' />
+                        <button>{"VERIFY LAB TEST (CLICK ME)"}</button>
+                    </form>
+                    {msg === 200 && <div className="lab-notification">
+                        <div className="lab-notify-icon">
+                            <img src={successIcon} alt="" />
+                        </div>
+                        <h4>THIS LAB TEST IS VERIFIED.</h4>
+                    </div>}
+                    {msg === 401 && <div className="lab-notification lab-notification-error">
+                        <div className="lab-notify-icon">
+                            <img src={errIcon} alt="" />
+                        </div>
+                        <h4>{`THE ORDER NUMBER IS REJECTED. THIS DOESN’T EXIST.`}</h4>
+                    </div>}
+                </div>
             </div>
         </section>
     )
